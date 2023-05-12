7754180a8c40ac219dcd26e64cc1394797026aa2
ani chukwuebuka <82764899+anichukwuebuka65@users.noreply.github.com>
2023-05-12 12:14:40 +0100

    Update VideoContainer.jsx

--- src/components/Video/VideoContainer.jsx
+++ src/components/Video/VideoContainer.jsx
@@ -4,10 +4,10 @@ import { localStream, remoteStream } from "./RTCPeerConn";
 
 export default function VideoContainer() {
   return (
-    <div className="flex flex-col justify-center h-screen bg-[#111010]">
+    <div className="flex flex-col justify-center h-screen bg-[#000]">
       <div className="relative  md:max-h-screen rounded-sm overflow-hidden">
         <Video stream={localStream} />
-        <div className="border border-white absolute w-1/4 md:top-8 top-4 lg:left-16 left-4 rounded-lg overflow-hidden">
+        <div className="border border-white absolute w-3/4 md:w-1/4 md:top-8 top-6 lg:left-16 left-4 rounded-lg overflow-hidden">
           <Video stream={remoteStream} />
         </div>
       </div>
