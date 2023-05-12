89f749f2f06e796ad7c521682274f41177ba3ce8
ani chukwuebuka <82764899+anichukwuebuka65@users.noreply.github.com>
2023-05-12 12:19:49 +0100

    Update Video.jsx

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -12,7 +12,6 @@ export default function Video({ stream }) {
         className=" bg-[#000] h-full md:object-contain"
         ref={videoRef}
         playsInline
-        autoPlay
       ></video>
     </>
   );
