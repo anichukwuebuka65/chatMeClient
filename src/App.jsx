efa5dd78142028876dcd631d9aaf370b33c5fb3b
anichukwuebuka <anichukwuebuka65@gmail.com>
2023-05-28 17:08:11 +0100

    added data channel

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const prodUrl = "https://chatmebackend.onrender.com";
 
 function App() {
   const [page, setPage] = useState("login");
-  const { wsConn, subscribe, subscribers } = useWebSocket(localUrl);
+  const { wsConn, subscribe, subscribers } = useWebSocket(prodUrl);
   const [roomId, setRoomId] = useState(1);
   const [iceServers, setIceServers] = useState([]);
 
